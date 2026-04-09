91e38107afd76212fdd3f8203f895fb094b9835a
ASAPU HARISH <141323054+harishasapu@users.noreply.github.com>
2026-04-09 18:20:43 +0530

    Add AWS provider and EC2 instance configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,44 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+locals {
+  instances = {
+    instance1 = { ami = data.aws_ami.ubuntu.id, instance_type = "t3.micro" }
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ec2"
+  public_key = file(var.public_key)
+}
+
+resource "aws_instance" "this" {
+  for_each                    = local.instances
+  ami                         = each.value.ami
+  instance_type               = each.value.instance_type
+  key_name                    = aws_key_pair.ssh_key.key_name
+  associate_public_ip_address = true
+
+  tags = { Name = each.key }
+}
+
+output "aws_instances" {
+  value = [for instance in aws_instance.this : instance.public_ip]
+}
