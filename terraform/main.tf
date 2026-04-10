15f1beb69094e04243281928c4e60405a047a352
ASAPU HARISH <141323054+harishasapu@users.noreply.github.com>
2026-04-10 14:25:10 +0530

    Add two new EC2 instances to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,14 @@ locals {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t3.micro"
     }
+    instance2 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t3.micro"
+    }
+    instance3 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t3.micro"
+    }
   }
 }
 
